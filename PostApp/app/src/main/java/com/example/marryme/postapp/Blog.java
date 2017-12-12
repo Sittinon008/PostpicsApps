99b2a4843107e50331db5da833afdc92fa6d370f
Sittinon <sittinon008@github.com>
2017-12-12 15:30:10 +0700

    save

---
+++ PostApp/app/src/main/java/com/example/marryme/postapp/Blog.java
@@ -0,0 +1,45 @@
+package com.example.marryme.postapp;
+
+/**
+ * Created by STD on 12/12/2560.
+ */
+
+public class Blog {
+    private String title;
+    private String desc;
+    private String image;
+
+    public Blog(){
+
+    }
+
+    public Blog(String title, String desc, String image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
